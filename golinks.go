64270e6ab4c2d3802eb4c103c2b211036a996491
David Fryer <dfryer1193@gmail.com>
2024-05-22 11:28:33 -0400

    Update usage and use parser/handler implementations

--- golinks.go
+++ golinks.go
@@ -5,18 +5,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/dfryer1193/golinks/internal/handler"
+	"github.com/dfryer1193/golinks/internal/links"
 )
 
 func main() {
 	var port int
-	var configDir string
 	var configFile string
-	configDir := os.Getenv("XDG_CONFIG_HOME")
-	port := *flag.Int("port", 8080, "The port to listen on")
-	configFile := flag.String("config", configDir+"links", "Location of the config file")
-	fmt.Printf("Starting http server:\nListening on port :%d\nUsing config file: %s", port, *configFile)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+	flag.IntVar(&port, "port", 8080, "The port to listen on")
+	flag.StringVar(&configFile, "config", "", "Location of the config file")
+
+	fmt.Printf("Starting http server:\nListening on port :%d\n", port)
+
+	redirector := handler.NewHandler(links.NewLinkMap(configFile))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), *redirector); err != nil {
 		log.Fatal(err)
 	}
 }
