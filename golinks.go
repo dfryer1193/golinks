503fe742d11c2599fe7765c5eb3dfb5c9bd39671
David Fryer <dfryer1193@gmail.com>
2024-07-29 13:02:47 -0400

    Add storage & loglevel documentation

--- golinks.go
+++ golinks.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/dfryer1193/golinks/internal/links/storage"
 	"net/http"
 	"os"
 	"strings"
@@ -23,6 +24,11 @@ Usage: golinks [-port 8080] [-config ./links]
 
 -h                                      Show this help message
 -port <number>                          The port to listen on (default: 8080)
+-storage <FILE|NONE>					The type of storage to use for persistence.
+										Defaults to "FILE". Storage types:
+											* NONE: Provides no persistence
+											* FILE: Persists shortcut entries to the
+													file specified by the -config option
 -config <absolute path to config file>  The path to the preferred config file.
                                         If this file is not present, falls back
                                         to default locations in the following
@@ -30,6 +36,7 @@ Usage: golinks [-port 8080] [-config ./links]
                                             * "./links"
                                             * "~/.config/golinks/links"
                                             * "/etc/golinks/links"
+-level <loglevel>						The loglevel to log at. Defaults to "INFO"
 
 Config format:
 The config file is a simple plaintext file consisting of one key/value pair per
@@ -47,10 +54,12 @@ respected, though full paths are.
 
 func main() {
 	var port int
+	var storageTypeString string
 	var configFile string
 	var stringLogLevel string
 	flag.IntVar(&port, "port", 8080, "The port to listen on")
-	flag.StringVar(&configFile, "config", "", "Location of the config file")
+	flag.StringVar(&storageTypeString, "storage", "FILE", "The type of storage to use for persistence")
+	flag.StringVar(&configFile, "config", "", "Location of the config file. Ignored if storageType is 'NONE'")
 	flag.StringVar(&stringLogLevel, "level", "INFO", "The level to log at")
 	flag.Usage = help
 
@@ -63,10 +72,11 @@ func main() {
 	level := getLevelFromArg(stringLogLevel)
 	log.Info().Str("loglevel", level.String()).Msg("Setting log level...")
 	zerolog.SetGlobalLevel(level)
+	storageType := storage.FromString(storageTypeString)
 
 	log.Info().Int("port", port).Msg("Starting http server")
 
-	redirector := handler.NewGolinkHandler(links.NewLinkMap(configFile))
+	redirector := handler.NewGolinkHandler(links.NewLinkMap(storageType, configFile))
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), redirector); err != nil {
 		log.Fatal().Err(err)
 	}
--- internal/links/storage/common.go
+++ internal/links/storage/common.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+	"github.com/rs/zerolog/log"
+	"strings"
+)
+
 type Storage interface {
 	Read() map[string]string
 	Put(key string, target string)
@@ -14,6 +19,23 @@ const (
 	NONE StorageType = iota
 	FILE
 )
+
+func (st StorageType) String() string {
+	return [...]string{"NONE", "FILE"}[st]
+}
+
+func FromString(s string) StorageType {
+	sanitized := strings.ToUpper(s)
+	switch sanitized {
+	case "NONE":
+		return NONE
+	case "FILE":
+		return FILE
+	default:
+		log.Fatal().Str("requestedStorageType", s).Msg("Storage type not recognized")
+	}
+	return FILE
+}
 
 type ParseError struct{}
 
