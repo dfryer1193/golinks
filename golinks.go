4a805fa800128a4877f1cf56bf15bb682049cdd7
David Fryer <dfryer1193@gmail.com>
2024-07-29 20:34:54 -0400

    Fix helptext width

--- golinks.go
+++ golinks.go
@@ -3,11 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/dfryer1193/golinks/internal/links/storage"
 	"net/http"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/dfryer1193/golinks/internal/links/storage"
 
 	"github.com/dfryer1193/golinks/internal/handler"
 	"github.com/dfryer1193/golinks/internal/links"
@@ -24,11 +25,13 @@ Usage: golinks [-port 8080] [-config ./links]
 
 -h                                      Show this help message
 -port <number>                          The port to listen on (default: 8080)
--storage <FILE|NONE>                    The type of storage to use for persistence.
-                                        Defaults to "FILE". Storage types:
+-storage <FILE|NONE>                    The type of storage to use for
+                                        persistence. Defaults to "FILE". Storage
+                                        types:
                                             * NONE: Provides no persistence
-                                            * FILE: Persists shortcut entries to the
-                                                    file specified by the -config option
+                                            * FILE: Persists shortcut entries to
+                                                    the file specified by the
+                                                    -config option
 -config <absolute path to config file>  The path to the preferred config file.
                                         If this file is not present, falls back
                                         to default locations in the following
@@ -36,7 +39,8 @@ Usage: golinks [-port 8080] [-config ./links]
                                             * "./links"
                                             * "~/.config/golinks/links"
                                             * "/etc/golinks/links"
--level <loglevel>                       The loglevel to log at. Defaults to "INFO"
+-level <loglevel>                       The loglevel to log at. Defaults to
+                                        "INFO"
 
 Config format:
 The config file is a simple plaintext file consisting of one key/value pair per
@@ -45,8 +49,7 @@ line, separated by spaces, like so:
     test https://www.google.com
 
 The value of the pair must be a full web address. Query params are not
-respected, though full paths are.
-`
+respected, though full paths are.`
 
 	fmt.Println(helptext)
 	os.Exit(0)
